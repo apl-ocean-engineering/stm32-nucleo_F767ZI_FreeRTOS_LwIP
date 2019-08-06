112f9cf3c02ce6c68d10855c38e825542eed0b8e
Aaron Marburg <amarburg@uw.edu>
2019-08-06 08:56:31 -0700

    First commit of code that compiles.

--- Inc/app_ethernet.h
+++ Inc/app_ethernet.h
@@ -1,8 +1,8 @@
 /**
   ******************************************************************************
-  * @file    LwIP/LwIP_HTTP_Server_Netconn_RTOS/Inc/main.h 
+  * @file    LwIP/LwIP_HTTP_Server_Netconn_RTOS/Inc/app_ethernet.h 
   * @author  MCD Application Team
-  * @brief   Header for main.c module
+  * @brief   Header for app_ethernet.c module
   ******************************************************************************
   * @attention
   *
@@ -44,48 +44,39 @@
   */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef __MAIN_H
-#define __MAIN_H
+#ifndef __APP_ETHERNET_H
+#define __APP_ETHERNET_H
 
 #ifdef __cplusplus
  extern "C" {
 #endif
 
 /* Includes ------------------------------------------------------------------*/
-#include "stm32f7xx_hal.h"
-#include "stm32f7xx_nucleo_144.h"
+#include "lwip/netif.h"
 
 /* Exported types ------------------------------------------------------------*/
-/* Exported constants --------------------------------------------------------*/ 
-#define USE_DHCP       /* enable DHCP, if disabled static address is used*/
- 
-/*Static IP ADDRESS*/
-#define IP_ADDR0   192
-#define IP_ADDR1   168
-#define IP_ADDR2   0
-#define IP_ADDR3   10
+/* Exported constants --------------------------------------------------------*/
+/* DHCP process states */
+#define DHCP_OFF                   (uint8_t) 0
+#define DHCP_START                 (uint8_t) 1
+#define DHCP_WAIT_ADDRESS          (uint8_t) 2
+#define DHCP_ADDRESS_ASSIGNED      (uint8_t) 3
+#define DHCP_TIMEOUT               (uint8_t) 4
+#define DHCP_LINK_DOWN             (uint8_t) 5
    
-/*NETMASK*/
-#define NETMASK_ADDR0   255
-#define NETMASK_ADDR1   255
-#define NETMASK_ADDR2   255
-#define NETMASK_ADDR3   0
-
-/*Gateway Address*/
-#define GW_ADDR0   192
-#define GW_ADDR1   168
-#define GW_ADDR2   0
-#define GW_ADDR3   1 
-
 /* Exported macro ------------------------------------------------------------*/
-/* Exported functions ------------------------------------------------------- */  
-
+/* Exported functions ------------------------------------------------------- */
+void User_notification(struct netif *netif);
+#ifdef USE_DHCP
+void DHCP_thread(void const * argument);
+#endif
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* __MAIN_H */
+#endif /* __APP_ETHERNET_H */
 
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
+
--- Src/syscalls.c
+++ Src/syscalls.c
@@ -31,38 +31,38 @@ extern int __io_getchar(void) __attribute__((weak));
 
 
 
-caddr_t _sbrk(int incr)
-{
-	extern char end asm("end");
-	static char *heap_end;
-	char *prev_heap_end,*min_stack_ptr;
-
-	if (heap_end == 0)
-		heap_end = &end;
-
-	prev_heap_end = heap_end;
-
-#ifdef FreeRTOS
-	/* Use the NVIC offset register to locate the main stack pointer. */
-	min_stack_ptr = (char*)(*(unsigned int *)*(unsigned int *)0xE000ED08);
-	/* Locate the STACK bottom address */
-	min_stack_ptr -= MAX_STACK_SIZE;
-
-	if (heap_end + incr > min_stack_ptr)
-#else
-	if (heap_end + incr > stack_ptr)
-#endif
-	{
-//		write(1, "Heap and stack collision\n", 25);
-//		abort();
-		errno = ENOMEM;
-		return (caddr_t) -1;
-	}
-
-	heap_end += incr;
-
-	return (caddr_t) prev_heap_end;
-}
+//caddr_t _sbrk(int incr)
+//{
+//	extern char end asm("end");
+//	static char *heap_end;
+//	char *prev_heap_end,*min_stack_ptr;
+//
+//	if (heap_end == 0)
+//		heap_end = &end;
+//
+//	prev_heap_end = heap_end;
+//
+//#ifdef FreeRTOS
+//	/* Use the NVIC offset register to locate the main stack pointer. */
+//	min_stack_ptr = (char*)(*(unsigned int *)*(unsigned int *)0xE000ED08);
+//	/* Locate the STACK bottom address */
+//	min_stack_ptr -= MAX_STACK_SIZE;
+//
+//	if (heap_end + incr > min_stack_ptr)
+//#else
+//	if (heap_end + incr > stack_ptr)
+//#endif
+//	{
+////		write(1, "Heap and stack collision\n", 25);
+////		abort();
+//		errno = ENOMEM;
+//		return (caddr_t) -1;
+//	}
+//
+//	heap_end += incr;
+//
+//	return (caddr_t) prev_heap_end;
+//}
 
 /*
  * _gettimeofday primitive (Stub function)
